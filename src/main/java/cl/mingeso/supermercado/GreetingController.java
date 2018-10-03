22e9e2de0fde5d6eb0bb0a903cbe1d6aa72ef8c4
carlos sepúlveda <carlos.sepulveda.a@usach.cl>
2018-10-03 15:51:29 -0400

    controlador

--- src/main/java/cl/mingeso/supermercado/GreetingController.java
+++ src/main/java/cl/mingeso/supermercado/GreetingController.java
@@ -1,9 +1,12 @@
-@RestController
+package cl.mingeso.supermercado;
+
+
 public class GreetingController {
 
   @GetMapping("/supemercado/api/v1/products/{name}")
-  /*public String hi(@PathVariable String name) {
-  	return "Hi " + name;
-  }*/
+
+  public String hi(@PathVariable String name) {
+  	System.out.println( "Hi " + name);
+  }
 
 }
